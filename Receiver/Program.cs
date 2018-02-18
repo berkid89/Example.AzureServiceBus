d2cda32bf4fab1ce14c394c74815ccc43410d4e9
berkid89 <berkid89@gmail.com>
2018-02-18 18:37:42 +0100

    initial commit

---
+++ Receiver/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://jaystacktest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=h8NC+jTTMMW8vX4sr6ksg26iH+UBnYzLn2rZXrTZndg=";
+        const string QueueName = "mytestqueue";
+        static IQueueClient queueClient;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            RegisterOnMessageHandlerAndReceiveMessages();
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            try
+            {
+                //throw new DivideByZeroException();
+                Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+                await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+            }
+            catch (Exception ex)
+            {
+                await queueClient.DeadLetterAsync(message.SystemProperties.LockToken, "error occured!", ex.Message);
+            }
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://jaystacktest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=h8NC+jTTMMW8vX4sr6ksg26iH+UBnYzLn2rZXrTZndg=";
+        const string QueueName = "mytestqueue";
+        static IQueueClient queueClient;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            // Send Messages
+            await SendMessagesAsync(new
+            {
+                id = 1,
+                name = "Test thing 1",
+                anotherThings = new List<string>() { "apple", "chair", "dog" }
+            });
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+        }
+
+        static async Task SendMessagesAsync(object msgBody)
+        {
+            try
+            {
+                var msgAsJson = JsonConvert.SerializeObject(msgBody);
+                var message = new Message(Encoding.UTF8.GetBytes(msgAsJson));
+
+                Console.WriteLine($"Sending message: {msgAsJson}");
+
+                // Send the message to the queue
+                await queueClient.SendAsync(message);
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
